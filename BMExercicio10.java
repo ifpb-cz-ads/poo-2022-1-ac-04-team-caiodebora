82aa49cb537793accab2253759a08b717be8c10b
Débora Camilly <debcamilly00@gmail.com>
2022-06-03 17:59:16 -0300

    Adiciona o exercicio 10 do livro Batista e Moraes

---
+++ BMExercicio10.java
@@ -0,0 +1,14 @@
+10.
+public class BMExercicio10 {
+  public static void main(String[] args) {
+    int k = 0;
+    int x = 10;
+
+    do {
+      System.out.println(x);
+      k++;
+    } while (k < 20);
+  }
+}
+
+ 
